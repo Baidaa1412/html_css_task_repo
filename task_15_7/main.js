cc5fe614e27ccc394a8344d3dce7e984658e442d
baidaa1412 <baidaaalkhalaf14@gmail.com>
2023-07-16 00:24:46 +0300

    15

---
+++ task_15_7/main.js
@@ -0,0 +1,12 @@
+
+var container = document.getElementById("hello-container");
+container.style.width = "400px";
+container.style.height = "70px";
+container.style.backgroundColor = "yellow";
+container.style.border = "2px solid black";
+
+var text = document.getElementById("hello-text");
+text.style.textAlign = "center";
+text.style.marginTop = "20px";
+text.style.fontSize = "24px";
+text.style.fontWeight="bold";
